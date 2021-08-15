d66fdf92d7240670f4e94b2bb2d04fbccf470fda
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-08-15 14:32:23 -0300

    Creating a function to change background color of BTN

--- Scenes/BTN/BTN.gd
+++ Scenes/BTN/BTN.gd
@@ -2,22 +2,32 @@ extends TextureButton
 
 func get_FRAME() -> FRAME:
 	var __FRAME: FRAME = $FRAME;
-	assert(__FRAME);
+	assert(__FRAME, "ERRO ON LINE 3");
 	return __FRAME;
 
+func get_LABEL() -> Label:
+	var __LABEL: Label = $Label;
+	assert(__LABEL, "ERRO ON LINE 8");
+	return __LABEL;
+
+func set_bg_color(__color: Color) -> void:
+	get_FRAME().get_bg().self_modulate = __color;
+
 func _on_mouse_entered() -> void:
-	get_FRAME().get_border().self_modulate = Color.yellow;
+	get_FRAME().get_border().self_modulate = Color.red;
 
 func _on_mouse_exited() -> void:
 	get_FRAME().get_border().self_modulate = Color.white;
 
 func _on_pressed() -> void:
+	get_LABEL().text = "Hello!";
 	print("Button Pressed!");
 
 func _ready() -> void:
 	connect("mouse_entered",self, "_on_mouse_entered");
 	connect("mouse_exited", self, "_on_mouse_exited");
 	connect("pressed",self,"_on_pressed");
+	set_bg_color(Color.green);
 
 
 
--- Scenes/FRAME/FRAME.gd
+++ Scenes/FRAME/FRAME.gd
@@ -5,5 +5,10 @@ class_name FRAME
 
 func get_border() -> NinePatchRect:
 	var __border: NinePatchRect = find_node("Border");
-	assert(__border);
+	assert(__border, "ERRO ON LINE 6");
 	return __border;
+
+func get_bg() -> NinePatchRect:
+	var __bg: NinePatchRect = find_node("BG");
+	assert(__bg, "ERRO ON LINE 11");
+	return __bg;
